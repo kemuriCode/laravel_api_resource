bfe667ea653cace51dad2fbd988473244a15497f
t0shi <t0shi@null.computer>
2018-12-25 17:59:32 +0100

    fix bugs about create db

--- database/migrations/2018_12_25_155050_create_articles_table.php
+++ database/migrations/2018_12_25_155050_create_articles_table.php
@@ -15,7 +15,6 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
             $table->string('title');
             $table->text('body');
             $table->timestamps();
